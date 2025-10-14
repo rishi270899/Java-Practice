75fe2a60cd0ce08e45549e32c8b1028c340afb6a
Rishi Goyal <rishig516@gmail.com>
2025-10-14 10:39:48 +0530

    Folder setup

--- practice/src/javainonevideo/datatypes/Test.java
+++ practice/src/javainonevideo/datatypes/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args){
         System.out.println("This is a number : " + (6+2));
         System.out.println("This is a number : " + (6+2));
+        System.out.println("This is a number : " + (6+2));
 
     }
 }
